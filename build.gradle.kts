f409e1a677efec90b28318ae74524122eb04d6e7
artem <dorogaada@yandex.ru>
2025-05-06 12:08:13 +0300

    +room

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,10 +1,17 @@
 import org.jetbrains.compose.desktop.application.dsl.TargetFormat
 
+
+val roomVersion = "2.7.1"
+val sqliteVersion = "2.5.0-alpha01"
+
 plugins {
     kotlin("jvm")
     id("org.jetbrains.compose")
     id("org.jetbrains.kotlin.plugin.compose")
+    id("androidx.room").version("2.7.1")
+    id("com.google.devtools.ksp").version("1.8.10-1.0.9")
 }
+
 
 group = "com.example"
 version = "1.0-SNAPSHOT"
@@ -21,6 +28,11 @@ dependencies {
     // (in a separate module for demo project and in testMain).
     // With compose.desktop.common you will also lose @Preview functionality
     implementation(compose.desktop.currentOs)
+
+    implementation("androidx.room:room-runtime:$roomVersion")
+    implementation("androidx.sqlite:sqlite-bundled:$sqliteVersion")
+    implementation("androidx.room:room-gradle-plugin:$roomVersion")
+    implementation("androidx.room:room-compiler:$roomVersion")
 }
 
 compose.desktop {
